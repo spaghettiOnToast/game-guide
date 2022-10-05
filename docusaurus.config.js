904ddabfc9a5e2bc18cdf1cebf13c08dca26d70d
Lord Squiddy <squiddy953@gmail.com>
2022-10-05 17:41:07 +1100

    homepage, styling

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'The Lords Guidebook',
+  tagline: 'Bibliotheca DAO',
+  url: 'https://guide.bibliothecadao.xyz/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <div className="container py-20 text-left">
+      <div className="mx-auto sm:w-1/2">
+        <img className="rounded-xl" src="/img/mobius.jpeg" alt="" />
+        <span className="italic"></span>
+        <p className="pt-8 mx-auto sm:text-3xl">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+          <Link
+            className="px-4 py-2 tracking-widest uppercase rounded shadow bg-black/20"
+            to="/docs/the-lords-guidebook"
+          >
+            Read The Lords Guidebook
+          </Link>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="The Lords Guidebook"
+    >
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
